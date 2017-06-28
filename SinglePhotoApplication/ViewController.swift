416e4156ccf1a8a9bcf90e995ef390df2ba2c6f5
Savannah McCoy <savannahmccoy@Savannahs-MacBook-Pro.local>
2017-06-27 18:15:47 -0700

    Initial Commit

---
+++ SinglePhotoApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SinglePhotoApplication
+//
+//  Created by Savannah McCoy on 6/27/17.
+//  Copyright © 2017 Savannah McCoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
