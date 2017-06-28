5acbe30e8c5b429e8f96cba68eabd9108204f6ca
Savannah McCoy <savannahmccoy@Savannahs-MacBook-Pro.local>
2017-06-27 18:21:58 -0700

    First commit

--- SinglePhotoApplication/ViewController.swift
+++ SinglePhotoApplication/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        imageView.image = UIImage(named: "image.jpg")
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
